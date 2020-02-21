c722313bc840c87a98b41d510ebb0a859f1ac56c
Birkhoff <juniorthx@gmail.com>
2020-02-21 02:39:27 -0800

    Updated Container

--- src/Container.jsx
+++ src/Container.jsx
@@ -4,16 +4,21 @@ import TimerController from './TimerController';
 import {useSelector, useDispatch} from 'react-redux'
 import { increment, decrement, reset, startTimer} from './store';
 import Timer from './Timer';
-//https://codesandbox.io/s/new-wr8ok
+
 function Container() {
     const breakLength=useSelector(state=>state.breakLength)
     const sessionLength=useSelector(state=>state.sessionLength)
     let minute=useSelector(state=>state.minute)
     const second=useSelector(state=>state.second)
     let isSession=useSelector(state=>state.isSession)
-    let timerON=useSelector(state=>state.timerON)
-    const dispatch=useDispatch()
-    
+    let isRunning=useSelector(state=>state.isRunning)
+    let isPaused=useSelector(state=>state.isPaused)
+    const dispatch=useDispatch();
+    const audio=document.getElementById('beep')
+    if(minute === 0 && second === 0){
+         audio.play();   
+    }
+
     return(
         <div className="container">
         <div className="clock-container">
@@ -34,10 +39,13 @@ function Container() {
            <Timer minute={minute}
                   second={second}
                   isSession={isSession}
-                  timerON={timerON}
+                  isRunning={isRunning}
+                  isPaused={isPaused}
                   handleClick={()=>dispatch(startTimer())}
                   reset={()=>dispatch(reset())}
            />
+          <audio id="beep" src="https://freesound.org/data/previews/153/153213_2499466-lq.mp3"></audio>
+          
           </div>
           <br />
         </div>
