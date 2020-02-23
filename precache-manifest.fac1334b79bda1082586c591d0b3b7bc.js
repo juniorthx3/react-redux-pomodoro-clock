1547c66d57966bd627b0f2ef940315560901be82
Birkhoff <juniorthx@gmail.com>
2020-02-22 21:29:31 -0800

    Updates

---
+++ precache-manifest.fac1334b79bda1082586c591d0b3b7bc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfd71394316b65ef4610902997e1797f",
+    "url": "/react-redux-pomodoro-clock/index.html"
+  },
+  {
+    "revision": "9aed15c71c7d40564c3e",
+    "url": "/react-redux-pomodoro-clock/static/css/2.b7c8eedf.chunk.css"
+  },
+  {
+    "revision": "9248bfb7d95ac84e83ec",
+    "url": "/react-redux-pomodoro-clock/static/css/main.06dc1829.chunk.css"
+  },
+  {
+    "revision": "9aed15c71c7d40564c3e",
+    "url": "/react-redux-pomodoro-clock/static/js/2.86c241a3.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-redux-pomodoro-clock/static/js/2.86c241a3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9248bfb7d95ac84e83ec",
+    "url": "/react-redux-pomodoro-clock/static/js/main.32c1fe2a.chunk.js"
+  },
+  {
+    "revision": "02af8af4892fd0524727",
+    "url": "/react-redux-pomodoro-clock/static/js/runtime-main.e615ee90.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-redux-pomodoro-clock/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-redux-pomodoro-clock/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-redux-pomodoro-clock/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-redux-pomodoro-clock/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-redux-pomodoro-clock/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
